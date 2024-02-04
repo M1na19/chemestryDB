66945931c3140ef173f52a32baf656fb197fe705
tuga06 <gabrieltudor2006@gmail.com>
2024-02-04 15:43:41 +0200

    new-frontend

--- FrontEnd/JS/main.js
+++ FrontEnd/JS/main.js
@@ -1,26 +1,8 @@
-let box1 = document.getElementById('atom-add1');
-//let box2 = document.getElementById('img-holder2');
-
-
-
 function spawn(atom1){
   var imgholds = document.getElementById('img-spawn');
   imgholds.innerHTML = '<img src="Atomi/' + atom1 + '" alt="Selected Image" style="cursor: pointer;">';
-  atom2 = atom1;
-}
-
-function killOrTransFirstImage() {
-  var img1 = document.getElementById('atom-holder1');
-  var img2 = document.getElementById('atom-holder2');
-  if(img2.innerHTML!=''){
-    img1.innerHTML = img2.innerHTML;
-    img2.innerHTML= '';
-  } else{
-    img1.innerHTML = '';
-  }
-}
-
-function killSecondImage() {
-  var img2 = document.getElementById('atom-holder2');
-  img2.innerHTML='';
+  //var ceva = document.getElementById('notite');
+  //ceva.style.display = 'block'; o unealta ce ne poate ajuta mai tarziu
+  var ns = document.getElementById('nume-sub');
+  ns.style.display = 'block';
 }
